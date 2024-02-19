8bf405033bb4d959c5dc148509c22c45b8517287
evabalu <baluchova.eva@gmail.com>
2024-02-19 21:30:37 +0100

    codereview refactor3

--- src/components/City/city.jsx
+++ src/components/City/city.jsx
@@ -9,7 +9,7 @@ export const City = ({ name, population, area, district, photo }) => {
         <p className='city__population'>Počet obyvateľov: {population}</p>
         <p className='city__area'>Rozloha: {area} m2</p>
         <p className='city__district'>Okres: {district}</p>
-        <img className='city__img' src={photo}/>
+        <img className='city__img' src={photo} alt=" "/>
         </div>
       </li>
     );
